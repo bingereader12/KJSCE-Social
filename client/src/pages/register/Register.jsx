a478d7e701af87f3e9042e306562d8615ac9481a
Devesh Jain <98257861+bingereader12@users.noreply.github.com>
2023-10-31 10:53:15 +0530

    Update Register.jsx

--- client/src/pages/register/Register.jsx
+++ client/src/pages/register/Register.jsx
@@ -2,6 +2,7 @@ import axios from "axios";
 import { useRef } from "react";
 import "./register.css";
 import { useNavigate } from "react-router"; // Import useNavigate instead of useHistory
+import { Link } from "react-router-dom";
 
 export default function Register() {
   const username = useRef();
@@ -71,7 +72,9 @@ export default function Register() {
             <button className="loginButton" type="submit">
               Sign Up
             </button>
+            <Link to="/login" style={{ textDecoration: "none" }}>
             <button className="loginRegisterButton">Log into Account</button>
+            </Link>
           </form>
         </div>
       </div>
